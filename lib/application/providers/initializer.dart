0cc8be5505ae244b28102fc07f1b440707c7811a
Clement Guyon <clement.guyon@orkester.fr>
2025-03-01 23:39:38 +0100

    fix: complete logs

--- lib/application/providers/initializer.dart
+++ lib/application/providers/initializer.dart
@@ -2,6 +2,7 @@ import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'package:squirrel/application/config/app_config.dart';
 import 'package:squirrel/application/providers/initializer.config.dart';
+import 'package:squirrel/foundation/utils/logger.util.dart';
 
 ///
 /// The injector instance
@@ -16,5 +17,19 @@ final GetIt injector = GetIt.instance;
     AppConfig,
   ],
 )
-Future<GetIt> initializeInjections(String environment) async =>
-    GetIt.I.init(environment: environment);
+Future<GetIt> initializeInjections(String environment) async {
+  try {
+    logInfo('Initialisation des injections...');
+    logInfo('Environnement: $environment');
+    
+    final getIt = await GetIt.I.init(
+      environment: environment,
+    );
+    
+    logInfo('Injections initialisées avec succès');
+    return getIt;
+  } catch (e, stackTrace) {
+    logException(e, stackTrace, 'Erreur lors de l\'initialisation des injections');
+    rethrow;
+  }
+}
